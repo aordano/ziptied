091c933544fda309a6e6029498bfaf8e1c312340
Ágata Ordano <ordanoagata@gmail.com>
2022-04-06 10:52:37 -0300

    fix(scoping): limit manually the API surface of components and bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export * from "./base";
-export * from "./component";
+export {
+    Component,
+    StatefulComponent,
+    DeepStatefulComponent,
+} from "./component";
 export * as Events from "./events";
 export * as Actions from "./actions";
 export * as Types from "./types";
