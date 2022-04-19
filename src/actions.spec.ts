9d1431a7fb737274e41c3a0acf686eca67a8c196
Ágata Ordano <ordanoagata@gmail.com>
2022-04-19 12:38:01 -0300

    test(actions): add test template

---
+++ src/actions.spec.ts
@@ -0,0 +1,41 @@
+import test from "ava";
+import { Primitives, Nodes } from "./actions";
+
+test("[Actions] [Primitive] arbitrarySet()", (t) => {
+    // TODO Should return value of the same type
+    // TODO Should return the same value
+});
+
+test("[Actions] [Primitive] arbitraryTransform()", (t) => {
+    // TODO Should return value of the same type
+    // TODO Should apply transform properly
+    // TODO Should reject non-function transforms
+});
+
+test("[Actions] [Basic actions] innerHTML()", (t) => {
+    // TODO
+});
+
+test("[Actions] [Basic actions] opacity()", (t) => {
+    // TODO
+});
+
+test("[Actions] [Basic actions] transform()", (t) => {
+    // TODO
+});
+
+test("[Actions] [Basic actions] appendChild()", (t) => {
+    // TODO
+});
+
+test("[Actions] [Basic actions] appendSibling()", (t) => {
+    // TODO
+});
+
+test("[Actions] [Basic actions] replace()", (t) => {
+    // TODO
+});
+
+test("[Actions] [Basic actions] remove()", (t) => {
+    // TODO
+});
