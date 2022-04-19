35979e00eb1c08aff4e57c949437320e316c8174
Ágata Ordano <ordanoagata@gmail.com>
2022-04-19 13:25:07 -0300

    test(base): add general goals that everything should met

--- src/base.spec.ts
+++ src/base.spec.ts
@@ -5,6 +5,17 @@ import { EditableNode, Node, State, StatefulNode, Stream } from "./base";
 // ======= EditableNode =======
 // ============================
 
+// * GOALS:
+// - Provide with reactivity to a regular HTMLElement.
+// - The reactivity should be direct, meaning that the element should
+// be able to directly modify itself when instructed to.
+// - The element should be directly modifiable in all its capacity, meaning that
+// all of its directly editable properties should be transformed.
+// - The element should remain present in the DOM, only being modified and not
+// supplanted entirely.
+// - The element should be able to register side effects so when it's transformed,
+//  it can trigger some consumer elsewhere to perform something else.
+
 test("[Base] [EditableNode] Factory functionality", (t) => {
     // TODO
 });
@@ -53,6 +64,10 @@ test("[Base] [EditableNode] Reaching side effect error hook", (t) => {
 // ======= State =======
 // =====================
 
+// * GOALS:
+// - Store data reactively and notify consumers when changes ocurr.
+// - The data should be readily available for everyone who queries or consumes it.
+
 test("[Base] [State] Factory functionality", (t) => {
     // TODO
 });
@@ -68,6 +83,21 @@ test("[Base] [State] Consuming state changes", (t) => {
 // ====================
 // ======= Node =======
 // ====================
+
+// * GOALS:
+// - Provide with reactivity to a regular HTMLElement.
+// - The reactivity should be action-based, meaning that the element should
+// register a set of recipes to perform when instructed to.
+// - This recipes should take some data, do something, transform the element somehow,
+// and return the modified element to be replaced in the DOM.
+// - The element should be able to register side effects so when it's transformed
+// by the recipes, it can trigger some consumer elsewhere to perform something else.
+// - The recipes should be freely attachable and detachable to the element, being able
+// to dynamically change the behavior it presents, even as a consequence of its own changes.
+// - Any recipe executed on the element should be able to do something in response
+// to errors.
+// - Any recipe executed on the element should be able to do something when it's no longer
+// needed and it has been detached from the element.
 
 test("[Base] [Node] Factory functionality", (t) => {
     // TODO
@@ -109,7 +139,7 @@ test("[Base] [Node] Reaching action error hook", (t) => {
     // TODO
 });
 
-test("[Base] [eNode] Reaching side effect lifecycle hook", (t) => {
+test("[Base] [Node] Reaching side effect lifecycle hook", (t) => {
     // TODO
 });
 
@@ -120,6 +150,15 @@ test("[Base] [Node] Reaching side effect error hook", (t) => {
 // ============================
 // ======= StatefulNode =======
 // ============================
+
+// * GOALS:
+// - Do everything the regular reactive element can do, plus:
+// - Provide each element with a state.
+// - The state should be reactive as well, behaving like the stated goals of
+// and instance of the previously defined State class.
+// - The state should be easily readable, writable, and consumable, externally.
+// - Side effects and some actions executed on this element should be aware of the
+// current state and be able to read, consume or modify it.
 
 test("[Base] [StatefulNode] Factory functionality", (t) => {
     // TODO
@@ -156,6 +195,10 @@ test("[Base] [StatefulNode] Firing a stateful side effect", (t) => {
 // ======================
 // ======= Stream =======
 // ======================
+
+// * GOALS:
+// - Pool data reactively and notify consumers when something is added to the pool.
+// - The data should be given to the consumer when the new element is added.
 
 test("[Base] [Stream] Factory functionality", (t) => {
     // TODO
