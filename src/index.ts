69b94aaa2a054578c9c65ec0d3cd58298ecdee67
Ágata Ordano <ordanoagata@gmail.com>
2022-04-25 09:23:08 -0300

    refactor(builtins): rename standard with builtin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./base"
-export * from "./standard"
+export * from "./builtin"
 export * as Component from "./component"
 export * as Events from "./events"
 export * as Actions from "./actions"
